2676e9d8db9e98cae71c92d3f7c1ab4c1086fb91
kappapig <kappapig91@gmail.com>
2024-04-02 22:14:18 +0300

    fix: wrong imports

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { Float, PresentationControls } from "@react-three/drei";
 import { useLoader } from "@react-three/fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 
-import just_a_girl from "./assets/models/just_a_girl/scene.gltf?raw";
+import just_a_girl from "./assets/models/just_a_girl/scene.gltf?url";
 
 function Scene() {
   const gltf = useLoader(GLTFLoader, just_a_girl);
