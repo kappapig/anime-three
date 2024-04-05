4f0e2a56a9199ebf92303d24542de3aa38a866ec
kappapig <kappapig91@gmail.com>
2024-04-06 00:39:25 +0300

    fix: bite base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/anime-three/",
   plugins: [react()],
   build: {
     outDir: "build",
